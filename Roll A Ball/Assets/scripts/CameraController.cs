161987d21709fa34a21440f85fc2eea05f11aae2
fcp3 <fcp3@njit.edu>
2017-06-11 17:04:29 -0400

    Added camera script to follow player object

---
+++ Roll A Ball/Assets/scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//references player object
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is guaranteed to run after all items have been processed to run
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+
+
+}
