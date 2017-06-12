50edc6393137de1cac2d92dccea1bea84b7b0345
fcp3 <fcp3@njit.edu>
2017-06-11 20:02:04 -0400

    Added scoring and game text

--- Roll A Ball/Assets/scripts/Movement.cs
+++ Roll A Ball/Assets/scripts/Movement.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour {
 
 	// Creates a Rigidbody object to get component of object
 	private Rigidbody rb;
+
+	private int pickupCnt;
+
+
 	// Variable that can be changed in Unity Engine
 	public float speed;
+	public Text scoreText;
+	public Text winText;
+
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		pickupCnt = 0;
+		SetScoreText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -32,9 +43,19 @@ public class Movement : MonoBehaviour {
 	// used to consume the pickups
 	void OnTriggerEnter (Collider other) {
 		
-		if (other.gameObject.CompareTag("PickUp"))
-			other.gameObject.SetActive(false);
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			pickupCnt = pickupCnt + 1;
+			SetScoreText ();
+		}
 
+	}
+
+	void SetScoreText () {
+		scoreText.text = "Score: " + pickupCnt.ToString ();
+		if (pickupCnt >= 12) {
+			winText.text = "You've won!";
+		}
 	}
 
 }
